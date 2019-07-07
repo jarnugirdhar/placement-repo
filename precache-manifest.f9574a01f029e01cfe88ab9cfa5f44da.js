2a7343a344949e7b7a14f4a67d0aa1669e926388
jarnu girdhar <jarnu.girdhar@addverb.in>
2019-07-07 11:58:54 +0530

    Updates

---
+++ precache-manifest.f9574a01f029e01cfe88ab9cfa5f44da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d353f2c0fb140fe0f81f8d83d5830e61",
+    "url": "/placement-repo/static/media/collage.d353f2c0.jpg"
+  },
+  {
+    "revision": "83c8293dbfe8ae17aba3",
+    "url": "/placement-repo/static/js/runtime~main.83c8293d.js"
+  },
+  {
+    "revision": "1148befc0576fb37d7a5",
+    "url": "/placement-repo/static/js/main.1148befc.chunk.js"
+  },
+  {
+    "revision": "bcfb3750899de99148a7",
+    "url": "/placement-repo/static/js/1.bcfb3750.chunk.js"
+  },
+  {
+    "revision": "1148befc0576fb37d7a5",
+    "url": "/placement-repo/static/css/main.e3de4c36.chunk.css"
+  },
+  {
+    "revision": "bcfb3750899de99148a7",
+    "url": "/placement-repo/static/css/1.ecac9602.chunk.css"
+  },
+  {
+    "revision": "eb278e2d26b6b91f6800bf644f2e0903",
+    "url": "/placement-repo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/placement-repo/precache-manifest.f9574a01f029e01cfe88ab9cfa5f44da.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/placement-repo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
